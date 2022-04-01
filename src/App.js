db513e629bad6d466a48e29a3872d93ec8f5ae5a
Sergii Gatezh <gatezh@users.noreply.github.com>
2022-04-01 09:29:23 -0400

    Add Sidebar import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Layout from "./Layout";
 import Navbar from "./components/navigation/Navbar";
 import Projects from "./components/Projects";
+import Sidebar from "./components/navigation/Sidebar";
 
 import "./styles.css";
 
@@ -12,6 +13,7 @@ export default function App() {
           <a href="/">Home</a>
           <a href="/projects">Projects</a>
         </Navbar>
+        <Sidebar />
         <Projects />
       </Layout>
     </div>
